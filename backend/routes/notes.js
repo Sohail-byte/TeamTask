9763a11b2acc5070fe8ed48b3c0bcaee31dcc993
Sohail-byte <sohailijaz09@gmail.com>
2025-08-23 21:16:40 +0500

    phase 1 completed, i have made a basic app from scratch and learnt a lot. I ahd used some help of copilot but just to learn not vibecode. i have retained or so i feel but at the same time i think i have learnt something as well. so yeah. now i have to refactor the code, make it more secure and whatnot. i will tackle those problems as i learn along the way.

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -73,6 +73,49 @@ notesRouter.get('/:id', isAuthenticated, async(req, res) => {
 })
 
 
+// route for updating the note 
+notesRouter.put('/:id/update', isAuthenticated, async (req, res) => {
+    const noteId = req.params.id
+    const {title, summary, content} = req.body
+
+
+
+    //idk what this does but i have read it builds the set object dynamicall
+    //it can be useful when you want to partailly update like if there are three fields in my case
+    //and if two fields are the same and only the third one has a change that needs to be updated in the database
+    console.log('PUT body:', req.body)
+    const $set = {}
+    //this is like doing homweork but you gotta do what you gotta do to learn
+    //this Object.proto type.has own property checks if the object has the property in it like the req.body is an object
+    //this checks it that object is empty or preset(if it isnt present then its empty i guess) to update the db with partial data or only
+    //with the data that is given in the request
+    //this was my explanantion and this is a hard concept and i have not even fully learned it but i have to look this up and how to define good routes and whatnot.
+    //
+    //this was like the validation phase maybe(defensive programming)
+    if (Object.prototype.hasOwnProperty.call(req.body, 'title')) $set.title = req.body.title
+    if (Object.prototype.hasOwnProperty.call(req.body, 'summary')) $set.summary = req.body.summary
+    if (Object.prototype.hasOwnProperty.call(req.body, 'content')) $set.content = req.body.content
+    //this checks if all the fields are empty
+    if (Object.keys($set).length === 0) {
+    return res.status(400).json({ msg: 'No fields provided' })
+  }
+    try{
+        const updated = await Note.findOneAndUpdate(
+            {_id: noteId},
+            {$set}
+        )
+        if(!updated){
+            return res.status(404).json({ msg: 'note not found' })
+        }
+        res.status(200).json({ msg: 'note updated successfully'})
+
+    }catch(e){
+        console.log(e)
+        res.status(500).json({msg: 'failed to update the note'})
+    }
+
+
+})
 
 
 
