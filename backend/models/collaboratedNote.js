ca07c789b9c2d1d48a6c3da306b6e5a44fd7f495
Sohail-byte <sohailijaz09@gmail.com>
2025-08-30 23:47:00 +0500

    made the collaborative notes model. this is only going to have the functionality to collab with 1 user at the time

---
+++ backend/models/collaboratedNote.js
@@ -0,0 +1,41 @@
+import mongoose from "mongoose"
+const collaboratedNoteSchema = new mongoose.Schema({
+    title: {
+            type: String,
+            required: true,
+            default: 'New Note',
+            unique: true
+        },
+        summary:{
+            type: String,
+            required: true
+        },
+        content: {
+            type: String,
+            default: ''
+        },
+        owner: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        dateCreated:{
+            type: Date,
+            default: new Date
+        },
+        collaborators: [{
+            user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'User'
+            }, 
+            permissions: {
+                type: String,
+                required: true,
+                enum: ['read', 'write', 'edit', 'view'],
+                default: 'view'
+            }
+        }]
+})
+
+
+const collaborativeNote = mongoose.model('collaborativeNote', collaboratedNoteSchema)
+export default collaborativeNote
