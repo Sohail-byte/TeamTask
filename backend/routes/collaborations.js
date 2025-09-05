2b2a95741da552e68a174b80580747a9ad465ede
Sohail-byte <sohailijaz09@gmail.com>
2025-09-05 16:24:59 +0500

    rearranged some code

--- backend/routes/collaborations.js
+++ backend/routes/collaborations.js
@@ -2,46 +2,8 @@ import express from "express";
 import collaborativeNote from '../models/collaboratedNote.js'
 import isAuthenticated from "../utils/middleware/authentication.js";
 import decodeJWTToken from "../utils/decodeToken.js";
-import mongoose from "mongoose";
 import User from '../models/user.js'
 const collaborationsRouter = express.Router()
-
-
-//im going to use the router for the pathc collaborations/create... and so on
-
-
-
-
-//i need to add users to the collabers and check what typoe of permissions they have then allot them 
-//controls like who can vieww and who can edit
-collaborationsRouter.get('/create',isAuthenticated, async(req, res) => {
-    res.render('notes/collaborativeNotes/newCollaborativeNote')
-})
-
-
-collaborationsRouter.post('/create',isAuthenticated, async(req, res) =>{
-    const {title, summary, content} = req.body
-
-    const decodedToken = decodeJWTToken(req.cookies.token)
-    const userId = decodedToken?.payload?.userId
-
-    try{
-        const newCollaborativeNote = await new collaborativeNote({
-            title,
-            summary,
-            content,
-            owner: userId
-        }).save()
-        res.status(201).redirect('/dashboard')
-    }catch(e){
-        console.log(e)
-        res.status(500).json({msg: e})
-    }
-})
-
-
-
-
 
 
 //this route retrieves all the collaborative notes the user has
@@ -64,6 +26,37 @@ collaborationsRouter.get('/',isAuthenticated, async(req, res) => {
 });
         // console.log(notesFound)
         res.json({notesFound})
+    }catch(e){
+        console.log(e)
+        res.status(500).json({msg: e})
+    }
+})
+
+
+
+
+//im going to use the router for the pathc collaborations/create... and so on
+//i need to add users to the collabers and check what typoe of permissions they have then allot them 
+//controls like who can vieww and who can edit
+collaborationsRouter.get('/create',isAuthenticated, async(req, res) => {
+    res.render('notes/collaborativeNotes/newCollaborativeNote')
+})
+
+
+collaborationsRouter.post('/create',isAuthenticated, async(req, res) =>{
+    const {title, summary, content} = req.body
+
+    const decodedToken = decodeJWTToken(req.cookies.token)
+    const userId = decodedToken?.payload?.userId
+
+    try{
+        const newCollaborativeNote = await new collaborativeNote({
+            title,
+            summary,
+            content,
+            owner: userId
+        }).save()
+        res.status(201).redirect('/dashboard')
     }catch(e){
         console.log(e)
         res.status(500).json({msg: e})
