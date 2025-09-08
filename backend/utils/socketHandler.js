9d692af12b1139acc0ec37d13a3c1f5f5639b40f
Sohail-byte <sohailijaz09@gmail.com>
2025-09-08 21:36:42 +0500

    made separate file for handling socket events

--- backend/utils/socketHandler.js
+++ backend/utils/socketHandler.js
@@ -1,14 +1,87 @@
 const sockethandling= async(io)=>{
     io.on('connection', (socket) => {
     io.emit('connected', 'connected successfully')
-
+    let RoomId
     socket.on('join-room', (roomId) => {
         socket.join(roomId)
         io.emit('joined-room', roomId)
+        RoomId = roomId
     })
-})
 
+
+    socket.on('noteTitleInput', (data) => {
+        socket.broadcast.to(RoomId).emit('noteTitleChange', data)
+        console.log(data)
+    })
+
+
+    socket.on('noteSummaryInput', (data) => {
+        socket.broadcast.to(RoomId).emit('noteSummaryChange', data)
+    })
+
+
+    socket.on('noteContentInput', (data) => {
+        socket.broadcast.to(RoomId).emit('noteContentChange', data)
+    })
+
+
+
+
+})
 }
+
+
+
+
+
+
+
+
+
+
+        // const handleTitleChange = async(content) =>{
+        //     socket.emit('noteTitleInput', JSON.stringify(content))
+        // }
+        // const handleSummaryChange = async(content) =>{
+        //     socket.emit('noteSummaryInput', JSON.stringify(content))
+        // }
+        // const handleContentChange = async(content) =>{
+        //     socket.emit('noteContentInput', JSON.stringify(content))
+        // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 export default sockethandling
