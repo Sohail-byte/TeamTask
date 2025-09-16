983d0127979321d5d0c31c4d3f9dad16e4c49d6f
Sohail-byte <sohailijaz09@gmail.com>
2025-09-16 22:24:13 +0500

    lets see added the lock feature. dont know how all this makes sense but im just tired at this point

--- backend/utils/socketHandler.js
+++ backend/utils/socketHandler.js
@@ -8,6 +8,8 @@ const sockethandling= async(io)=>{
         RoomId = roomId
     })
 
+    let roomLocks = {} //roomLock = { room : {socketId, userId}}
+
 
     socket.on('noteTitleInput', (data) => {
         socket.broadcast.to(RoomId).emit('noteTitleChange', data)
@@ -23,15 +25,54 @@ const sockethandling= async(io)=>{
         socket.broadcast.to(RoomId).emit('noteContentChange', data)
     })
 
+    socket.on('lock-request', () => {
+        const room = socket.rooms
+        if(!roomLocks[room] || roomLocks[room].userId === socket.id){
+            roomLocks[room] = {userId: socket.id}
+            socket.broadcast.to(room).emit('lock-status', {lockedBy: socket.userId})
+            startLockTimer(room, socket.userId)
+        }else {
+            socket.emit('lock-status', { lockedBy: roomLocks[RoomId].userId });
+        }
 
+    })
 
+    socket.on('release-lock', () => {
+    const room = socket.rooms
+    if (roomLocks[room] && roomLocks[room].userId === socket.userId) {
+      clearTimeout(roomLocks[room].timer);
+      delete roomLocks[room];
+      io.to(room).emit('lock-status', { lockedBy: null });
+    }
+    })
+
+    socket.on('typing', () => {
+    const room = socket.room;
+    if (roomLocks[room] && roomLocks[room].userId === socket.userId) {
+      resetLockTimer(room, socket.userId);
+    }
+    })
+
+    
 
 })
 }
 
 
+function startLockTimer(RoomId, userId) {
+  roomLocks[RoomId].timer = setTimeout(() => {
+    if (roomLocks[RoomId]?.userId === userId) {
+      delete roomLocks[RoomId];
+      io.to(RoomId).emit('lock-status', { lockedBy: null });
+    }
+  }, 20000); // 20 seconds
+}
 
 
+function resetLockTimer(RoomId, userId) {
+  clearTimeout(roomLocks[RoomId].timer);
+  startLockTimer(RoomId, userId);
+}
 
 
 
